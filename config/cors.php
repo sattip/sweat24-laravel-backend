b136ae4a618a7169461847b1f92d6bc2d9dbb875
George Pittas <gpittas@obstechnologies.com>
2025-12-01 10:00:01 +0000

    Auto commit - 2025-12-01 10:00:01

--- config/cors.php
+++ config/cors.php
@@ -19,9 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-       '*',
-    ],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
@@ -31,6 +29,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 
 ];
