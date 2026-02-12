ed78f35cb956a4f3b6e4a4ff413fe06e2d97ce4c
George Pittas <gpittas@obstechnologies.com>
2026-02-12 13:00:01 +0000

    Auto commit - 2026-02-12 13:00:01

--- routes/api.php
+++ routes/api.php
@@ -1199,7 +1199,7 @@ Route::prefix('v1/referrals')->group(function () {
             'data' => [
                 'user_name' => $user->name,
                 'referral_code' => $referralCode->code,
-                'referral_link' => "https://sweat24.obs.com.gr/invite/" . $referralCode->code,
+                'referral_link' => "https://api.sweat93.gr/invite/" . $referralCode->code,
                 'total_referrals' => $totalReferrals,
                 'next_tier' => $nextTier ? [
                     'name' => $nextTier->name,
